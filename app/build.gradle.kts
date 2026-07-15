b61582dfa0692144624306336aec5dc82751580d
Michael Bentz <michael@strigate.org>
2026-07-15 22:18:54 +0200

    fix: Suppress duplicate Compose BOM warning
    - Suppress `AvoidDuplicateDependencies` on required main and Android test BOM declarations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -287,6 +287,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     // Compose
+    @Suppress("AvoidDuplicateDependencies")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -333,6 +334,7 @@ dependencies {
     testImplementation(libs.json)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    @Suppress("AvoidDuplicateDependencies")
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     // Debug
